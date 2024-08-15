5d88923b4b8047be5951adf1eece0bb9c9721002
MandisaDev <rmandisa236@gmail.com>
2024-08-16 01:22:56 +0200

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -19,10 +19,9 @@ function generatePoem(event) {
   let context = "You are a romantic poem expert and love to write short poem.Your mission is to generate a 4 lines poem in basic html and separate each line with a <br />. make sure to follow user instructions. Do not include a tittle to the poem.Sign the poem with 'sheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning ";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
-
-  console.log("Generating poem");
-  console.log(`prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem); 
 
